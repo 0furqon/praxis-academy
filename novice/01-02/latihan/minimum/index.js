ac447f7ef9924b88a5f3001a5d6baad7e6de4b8b
Arif Furqon <muh.a.furqon@gmail.com>
2019-07-27 23:23:49 +0700

    latihan minimum

---
+++ novice/01-02/latihan/minimum/index.js
@@ -0,0 +1,11 @@
+function min (firstNumber, secondNumber){
+    if (firstNumber < secondNumber) {
+        return firstNumber;
+    } else {
+        return secondNumber;
+    }
+}
+
+console.log(min(10,-20));
+console.log(min(0,4));
+console.log(min(12,12));
