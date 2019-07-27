0527adc0e3181e8e0df164381c9cc8a75bfa9b75
Arif Furqon <muh.a.furqon@gmail.com>
2019-07-27 16:43:05 +0700

    looping a triangle

---
+++ novice/01-01/kasus/looping-a-triangle/index.js
@@ -0,0 +1,8 @@
+var karakter = "#"; // karakter yang ingin di tampilkan
+var segitiga = ""; // variable yang menampung string output
+var i = 7; // kedalaman segitiga
+
+for (var x = 1; x <=7; x++ ){
+    karakter += hastag; // menambahkan karakter setiap perulangan baris
+    console.log(karakter); // menampilkan output ke konsol
+}
